b71ba12863ddb91358a7db2abe2619eb4b6d3757
Daniel Bento <dcstro.infor@gmail.com>
2021-06-14 23:44:03 -0300

    ajustes classe livro

--- bancodedados.cpp
+++ bancodedados.cpp
@@ -32,7 +32,6 @@ bool bancodedados::isOpen() const
     return m_db.isOpen();
 }
 
-
 bool bancodedados::execute(QString str){
     QSqlQuery query;
     qDebug() << str;
@@ -60,140 +59,7 @@ QString bancodedados::executeReturn(QString str){
     return "0";
 
 }
-/*
-bool bancodedados::addBook(const livro& livro)
-{
-    bool success = false;
-        QSqlQuery queryAdd;
-        queryAdd.prepare("INSERT INTO livro (ISBN, nome, autor, categoria) VALUES (:ISBN, :nome, :autor, :categoria)");
-        queryAdd.bindValue(":ISBN", livro.isbn);
-        queryAdd.bindValue(":nome", livro.nome);
-        queryAdd.bindValue(":autor", livro.autor);
-        queryAdd.bindValue(":categoria", livro.categoria);
 
-        if(queryAdd.exec())
-        {
-            success = true;
-        }
-        else
-        {
-            qDebug() << "add livro failed: " << queryAdd.lastError();
-        }
-
-        queryAdd.finish();
-
-    return success;
-}
-
-bool bancodedados::removeBook(const QString& isbn)
-{
-    bool success = false;
-
-    if (bookExists(isbn))
-    {
-        QSqlQuery queryDelete;
-        queryDelete.prepare("DELETE FROM livro WHERE ISBN = (:isbn)");
-        queryDelete.bindValue(":isbn", isbn);
-        success = queryDelete.exec();
-
-        if(!success)
-        {
-            qDebug() << "remoção falhou: " << queryDelete.lastError();
-        }
-        queryDelete.finish();
-    }
-    else
-    {
-        qDebug() << "livro não existe";
-    }
-    return success;
-}
-
-bool bancodedados::updateBook(const livro& livro)
-{
-    bool success = false;
-        QSqlQuery queryAdd;
-        queryAdd.prepare("UPDATE livro SET nome = :nome, autor = :autor, categoria = :categoria WHERE ISBN = :ISBN");
-        queryAdd.bindValue(":ISBN", livro.isbn);
-        queryAdd.bindValue(":nome", livro.nome);
-        queryAdd.bindValue(":autor", livro.autor);
-        queryAdd.bindValue(":categoria", livro.categoria);
-
-        if(queryAdd.exec())
-        {
-            success = true;
-        }
-        else
-        {
-            qDebug() << "Update livro failed: " << queryAdd.lastError();
-        }
-
-        queryAdd.finish();
-
-    return success;
-}
-
-bool bancodedados::bookExists(const QString& isbn) const
-{
-    bool exists = false;
-
-    QSqlQuery checkQuery;
-    checkQuery.prepare("SELECT ISBN FROM livro WHERE ISBN = (:isbn)");
-    checkQuery.bindValue(":isbn", isbn);
-
-    if (checkQuery.exec())
-    {
-        if (checkQuery.next())
-        {
-            exists = true;
-        }
-    }
-    else
-    {
-        qDebug() << "ISBN não existe: " << checkQuery.lastError();
-    }
-
-    return exists;
-}
-
-/*
-livro bancodedados::returnBook(const QString& campo, const QString& valor){
-
-    livro aux_livro;
-    QSqlQuery checkQuery;
-    //ajustar forma de consulta
-
-    if(campo == "ISBN")
-        checkQuery.prepare("SELECT * FROM livro WHERE ISBN = :valor");
-    else if(campo == "Nome")
-        checkQuery.prepare("SELECT * FROM livro WHERE nome = :valor");
-    else if(campo == "Autor")
-        checkQuery.prepare("SELECT * FROM livro WHERE autor = :valor");
-    else if(campo == "Categoria")
-        checkQuery.prepare("SELECT * FROM livro WHERE categoria = :valor");
-
-    checkQuery.bindValue(":valor", valor);
-
-
-    if (checkQuery.exec())
-    {
-        if(checkQuery.next())
-        {
-            aux_livro.isbn = checkQuery.value("ISBN").toString();
-            aux_livro.nome = checkQuery.value("nome").toString();
-            aux_livro.autor = checkQuery.value("autor").toString();
-            aux_livro.categoria = checkQuery.value("categoria").toString();
-        }
-    }
-    else
-    {
-        qDebug() << "Não encontramos registros: " << checkQuery.lastError();
-    }
-    checkQuery.finish();
-    return aux_livro;
-}
-
-*/
 //para testes
 bool bancodedados::removeAllBooks()
 {
